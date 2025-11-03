afd0215dda0e1e44ddb2d66fed9808712e99df32
Edric Yahir <aguilarhernandezedricyahir@gmail.com>
2025-11-03 10:02:29 -0600

    Comprobar endpoints fucnionando

--- arroz/index.js
+++ arroz/index.js
@@ -1,47 +1,132 @@
-// "Guerrilla" Workshop - Distributed Applications-UPIITA-IPN
-
+var express = require("express");
+var app = express(); //Contenedor de Endpoints o WS Restful
 const { MongoClient } = require("mongodb");
+var client = 0;
 
-async function run() {
-  // TODO:
-  // Replace the placeholder connection string below with your
-  // Altas cluster specifics. Be sure it includes
-  // a valid username and password! Note that in a production environment,
-  // you do not want to store your password in plain-text here.
+var dbName = "";
+var collectionName = "";
+
+// Create references to the database and collection in order to run
+// operations on them.
+var database = 0;
+var collection = 0;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ⭐ AGREGAR ESTAS LÍNEAS AQUÍ ⭐
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+// ⭐ FIN DE LAS LÍNEAS NUEVAS ⭐
+function prepareDB() {
+  dbName = "myDatabase";
+  collectionName = "recipes";
+
+  // Create references to the database and collection in order to run
+  // operations on them.
+  database = client.db(dbName);
+  collection = database.collection(collectionName);
+}
+
+async function connectDB() {
   const uri =
     "mongodb+srv://edricaguilarhdz_db_user:WRZLzm0IR0uniW2N@cluster0.m4xir2m.mongodb.net/?appName=Cluster0";
 
-  // The MongoClient is the object that references the connection to our
-  // datastore (Atlas, for example)
-  const client = new MongoClient(uri);
+  client = new MongoClient(uri);
 
   // The connect() method does not attempt a connection; instead it instructs
   // the driver to connect using the settings provided when a connection
   // is required.
   await client.connect();
+}
 
-  // Provide the name of the database and collection you want to use.
-  // If the database and/or collection do not exist, the driver and Atlas
-  // will create them automatically when you first write data.
-  const dbName = "myDatabase";
-  const collectionName = "recipes";
+app.get("/", async function (request, response) {
+  r = {
+    message: "Nothing to send",
+  };
 
-  // Create references to the database and collection in order to run
-  // operations on them.
-  const database = client.db(dbName);
-  const collection = database.collection(collectionName);
+  response.json(r);
+});
 
-  /*
-   *  *** INSERT DOCUMENTS ***
-   *
-   * You can insert individual documents using collection.insert().
-   * In this example, we're going to create four documents and then
-   * insert them all in one call with collection.insertMany().
-   */
+/*
+Calling this service sending payload as parameters in URL: 
+https://typesofwebservices.noesierra.repl.co/serv001?id=Nope&token=2345678dhuj43567fgh&geo=123456789,1234567890
+*/
+app.get("/serv001", async function (req, res) {
+  const user_id = req.query.id;
+  const token = req.query.token;
+  const geo = req.query.geo;
 
+  r = {
+    user_id: user_id,
+    token: token,
+    geo: geo,
+  };
+
+  res.json(r);
+});
+
+/*
+Calling this service sending payload as parameters in URL: 
+https://typesofwebservices.noesierra.repl.co/serv001?id=Nope&token=2345678dhuj43567fgh&geo=123456789,1234567890
+*/
+app.get("/serv0010", async function (req, res) {
+  const user_id1 = req.query.id;
+  const token1 = req.query.token;
+  const geo1 = req.query.geo;
+
+  r1 = {
+    user_id: user_id1,
+    token: token1,
+    geo: geo1,
+  };
+
+  res.json(r1);
+});
+
+// Call this service sending payload in body: raw - json
+/*
+{
+    "id": "nope",
+    "token": "ertydfg456Dfgwerty",
+    "geo": "12345678,34567890"
+}
+*/
+app.post("/serv002", async function (req, res) {
+  const user_id = req.body.id;
+  const token = req.body.token;
+  const geo = req.body.geo;
+
+  r = {
+    user_id: user_id,
+    token: token,
+    geo: geo,
+  };
+
+  res.json(r);
+});
+
+/*
+Call this service sending parameter as a part of the URL
+https://typesofwebservices.noesierra.repl.co/serv003/1234567
+*/
+app.post("/serv003/:info", async function (req, res) {
+  const info = req.params.info;
+  let r = { info: info };
+  res.json(r);
+});
+
+app.post("/recipe/insert", async function (req, res) {
   const recipes = [
     {
-      name: "elotes",
+      name: "elotes cocidos",
       ingredients: [
         "corn",
         "mayonnaise",
@@ -51,151 +136,28 @@ async function run() {
       ],
       prepTimeInMinutes: 35,
     },
-    {
-      name: "loco moco",
-      ingredients: [
-        "ground beef",
-        "butter",
-        "onion",
-        "egg",
-        "bread bun",
-        "mushrooms",
-      ],
-      prepTimeInMinutes: 54,
-    },
-    {
-      name: "patatas bravas",
-      ingredients: [
-        "potato",
-        "tomato",
-        "olive oil",
-        "onion",
-        "garlic",
-        "paprika",
-      ],
-      prepTimeInMinutes: 80,
-    },
-    {
-      name: "fried rice",
-      ingredients: [
-        "rice",
-        "soy sauce",
-        "egg",
-        "onion",
-        "pea",
-        "carrot",
-        "sesame oil",
-      ],
-      prepTimeInMinutes: 40,
-    },
   ];
+
+  let result = "";
 
   try {
     const insertManyResult = await collection.insertMany(recipes);
     console.log(
       `${insertManyResult.insertedCount} documents successfully inserted.\n`,
     );
+    result = `${insertManyResult.insertedCount} documents successfully inserted.`;
   } catch (err) {
     console.error(
       `Something went wrong trying to insert the new documents: ${err}\n`,
     );
+    result = `Something went wrong trying to insert the new documents: ${err}`;
   }
+  let r = { result: result };
+  res.json(r);
+});
 
-  /*
-   * *** FIND DOCUMENTS ***
-   *
-   * Now that we have data in Atlas, we can read it. To retrieve all of
-   * the data in a collection, we call Find() with an empty filter.
-   * The Builders class is very helpful when building complex
-   * filters, and is used here to show its most basic use.
-   */
-
-  const findQuery = { prepTimeInMinutes: { $lt: 45 } };
-
-  try {
-    const cursor = await collection.find(findQuery).sort({ name: 1 });
-    await cursor.forEach((recipe) => {
-      console.log(
-        `${recipe.name} has ${recipe.ingredients.length} ingredients and takes ${recipe.prepTimeInMinutes} minutes to make.`,
-      );
-    });
-    // add a linebreak
-    console.log();
-  } catch (err) {
-    console.error(
-      `Something went wrong trying to find the documents: ${err}\n`,
-    );
-  }
-
-  // We can also find a single document. Let's find the first document
-  // that has the string "potato" in the ingredients list.
-  const findOneQuery = { ingredients: "potato" };
-
-  try {
-    const findOneResult = await collection.findOne(findOneQuery);
-    if (findOneResult === null) {
-      console.log(
-        "Couldn't find any recipes that contain 'potato' as an ingredient.\n",
-      );
-    } else {
-      console.log(
-        `Found a recipe with 'potato' as an ingredient:\n${JSON.stringify(
-          findOneResult,
-        )}\n`,
-      );
-    }
-  } catch (err) {
-    console.error(`Something went wrong trying to find one document: ${err}\n`);
-  }
-
-  /*
-   * *** UPDATE A DOCUMENT ***
-   *
-   * You can update a single document or multiple documents in a single call.
-   *
-   * Here we update the PrepTimeInMinutes value on the document we
-   * just found.
-   */
-  const updateDoc = { $set: { prepTimeInMinutes: 72 } };
-
-  // The following updateOptions document specifies that we want the *updated*
-  // document to be returned. By default, we get the document as it was *before*
-  // the update.
-  const updateOptions = { returnOriginal: false };
-
-  try {
-    const updateResult = await collection.findOneAndUpdate(
-      findOneQuery,
-      updateDoc,
-      updateOptions,
-    );
-    console.log(
-      `Here is the updated document:\n${JSON.stringify(updateResult.value)}\n`,
-    );
-  } catch (err) {
-    console.error(
-      `Something went wrong trying to update one document: ${err}\n`,
-    );
-  }
-
-  /*      *** DELETE DOCUMENTS ***
-   *
-   *      As with other CRUD methods, you can delete a single document
-   *      or all documents that match a specified filter. To delete all
-   *      of the documents in a collection, pass an empty filter to
-   *      the DeleteMany() method. In this example, we'll delete two of
-   *      the recipes.
-   */
-
-  const deleteQuery = { name: { $in: ["elotes", "fried rice"] } };
-  try {
-    const deleteResult = await collection.deleteMany(deleteQuery);
-    console.log(`Deleted ${deleteResult.deletedCount} documents\n`);
-  } catch (err) {
-    console.error(`Something went wrong trying to delete documents: ${err}\n`);
-  }
-
-  // Make sure to call close() on your client to perform cleanup operations
-  await client.close();
-}
-run().catch(console.dir);
+app.listen(3000, function () {
+  console.log("Aplicación ejemplo, escuchando el puerto 3000!");
+  connectDB();
+  prepareDB();
+});
